fb51809d832f94d443cdde01db9f7abdea78e2b5
Viktor Kuzminykh <simpat007@yandex.ru>
2020-08-19 17:54:03 +0300

    задача

---
+++ main.js
@@ -0,0 +1,22 @@
+const firstRow = "мама мыла раму";
+const secondRow = "собака друг человека";
+
+function getRow(firstRow, secondRow) {
+  let char = "а";
+  function getNumCh(char, str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] == char) {
+        count++;
+      }
+    }
+    return count;
+  }
+  const firstNum = getNumCh(char, firstRow);
+  const secondNum = getNumCh(char, secondRow);
+  if (firstNum > secondNum) {
+    return firstRow;
+  } else return secondRow;
+}
+
+console.log(getRow(firstRow, secondRow));
